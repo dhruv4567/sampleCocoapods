c251eaaf7d270d3632a6464697c8df7f94273ab3
dhruv4567 <khatridhruv4567@gmail.com>
2016-11-14 01:52:32 +0530

    Initial Commit

---
+++ sampleCocoapods/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  sampleCocoapods
+//
+//  Created by Dhruv Khatri on 14/11/16.
+//  Copyright © 2016 OneTouch. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
